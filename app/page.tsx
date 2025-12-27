0f11e0b22eb2cf2dc2f3b50b3d203a1b1615fdf9
infopermatacrane-ui <infopermatacrane@gmail.com>
2025-12-28 06:25:02 +0700

    add app page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Permata Crane</h1>
+      <p>Sewa Mobil Crane Profesional</p>
+      <p>Website sedang dalam pengembangan.</p>
+    </main>
+  );
+}
